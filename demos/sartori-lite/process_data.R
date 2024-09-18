e0edd9542820134cae1c769c7d59d53b98dd9494
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-09-18 15:53:10 +0000

    Built site for gh-pages

--- demos/sartori-lite/process_data.R
+++ demos/sartori-lite/process_data.R
@@ -1,5 +1,33 @@
-r = read.csv("ccp_ontology.csv") |> unique()
-r = r[r$Key != "",]
+library(stringr)
+# CCP ontology ----
 
-writeLines(r$Label, "ccp_labels.txt")
-writeLines(r$Description, "ccp_descriptions.txt")
+r = read.csv("raw_data/ccp_ontology.csv") |> unique()
+r = r[r$Key != "",]
+r$td = str_c(r$Label, ": ", r$Description)
+r$td_count = str_count(r$td)
+writeLines(r$td, "data/ccp_td.txt")
+writeLines(r$Label, "data/ccp_labels.txt")
+writeLines(r$Description, "data/ccp_descriptions.txt")
+
+# IDEA constitutional topics ontology ----
+
+idea_fpaths = list.files("raw_data", full.names = T, pattern = "Ontology")
+l = lapply(idea_fpaths, read.csv)
+df_idea = do.call(rbind, l)
+
+df_idea$td = str_c(df_idea$Topic, ": ", df_idea$Description)
+df_idea$td_count = str_count(df_idea$td)
+df_idea = df_idea[df_idea$td_count < 200,]
+
+writeLines(
+    c(r$td, df_idea$td), "data/merged_td.txt"
+)
+
+writeLines(
+    c(rep("CCP", nrow(r)), rep("IDEA", nrow(df_idea))),
+    "data/merged_td_ont.txt"
+)
+
+writeLines(df_idea$td, "data/idea_td.txt")
+writeLines(df_idea$Topic, "data/idea_labels.txt")
+writeLines(df_idea$Description, "data/idea_descriptions.txt")
